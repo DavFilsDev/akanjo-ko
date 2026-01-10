0f03ec7f7e02d5f568f726f1660aade74933753c
DavFilsDev <miharisoadavidfils@gmail.com>
2026-01-10 21:23:38 +0300

    feat(seed): add test users and admins with hashed passwords

--- backend/src/seed/index.ts
+++ backend/src/seed/index.ts
@@ -1,9 +1,10 @@
+import { seedUsers } from "./user.seed";
 import { seedProducts } from "./product.seed";
 
 export const runSeeds = async () => {
   try {
+    await seedUsers();
     await seedProducts();
-
   } catch (error) {
     console.error("Seeding failed", error);
   }
