aa4dc6026d0e32b018f95f3b67abdb83e7202b44
DavFilsDev <miharisoadavidfils@gmail.com>
2026-01-14 11:12:52 +0300

    feature(user): add removeCartItem function to remove an item inside a cart after verification and using removeCartItemService

--- backend/src/controllers/public/cart.controller.ts
+++ backend/src/controllers/public/cart.controller.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 import { AuthRequest } from "../../middleware/auth.middleware";
-import { getOrCreateCartService, addItemToCartService } from "../../services/public/cart.service";
+import { getOrCreateCartService, addItemToCartService, removeCartItemService } from "../../services/public/cart.service";
 
 export const getCurrentCart = async (
   req: AuthRequest,
@@ -60,6 +60,26 @@ export const addItemToCart = async (
     return res.status(400).json({
       success: false,
       message: error.message || "Failed to add item to cart",
+    });
+  }
+};
+
+export const removeCartItem = async (req: AuthRequest, res: Response) => {
+  try {
+    const itemId = Number(req.params.id);
+    const userId = req.user!.id;
+
+    const cart = await removeCartItemService(userId, itemId);
+
+    return res.status(200).json({
+      success: true,
+      message: "Item removed from cart",
+      data: cart,
+    });
+  } catch (error: any) {
+    return res.status(400).json({
+      success: false,
+      message: error.message || "Failed to remove item",
     });
   }
 };
