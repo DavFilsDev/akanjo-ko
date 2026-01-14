31e55f8337db28707db387893076ccffaa6325df
DavFilsDev <miharisoadavidfils@gmail.com>
2026-01-14 11:49:54 +0300

    feature(user): import updateCartItemQuantity function into cart.routes.ts with verb http path

--- backend/src/routes/public/cart.routes.ts
+++ backend/src/routes/public/cart.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCurrentCart, addItemToCart, removeCartItem } from "../../controllers/public/cart.controller";
+import { getCurrentCart, addItemToCart, removeCartItem, updateCartItemQuantity } from "../../controllers/public/cart.controller";
 import { authMiddleware } from "../../middleware/auth.middleware";
 
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.get("/", authMiddleware, getCurrentCart);
 router.post("/items", authMiddleware, addItemToCart);
 router.delete("/items/:id", authMiddleware, removeCartItem);
+router.patch("/items/:id", authMiddleware, updateCartItemQuantity);
 
 export default router;
